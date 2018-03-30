0eca1705685e6420ddbfcf8d150eb0f0f167cef6
AbdulbasitSaid <mrspeed518@gmail.com>
2018-03-30 16:34:53 +0100

    first Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { PostService } from './post.service';
+import { PostsComponent } from './posts/posts/posts.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    PostService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
